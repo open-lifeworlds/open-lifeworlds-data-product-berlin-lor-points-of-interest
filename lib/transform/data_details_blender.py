5c81b7c14287baa37840cb8060113c54a00ce599
Florian Schwanz <florian.schwanz@interoberlin.de>
2024-01-01 15:54:51 +0100

    chore: add data details blender #1

---
+++ lib/transform/data_details_blender.py
@@ -0,0 +1,93 @@
+import json
+import os
+
+import pandas as pd
+
+from lib.tracking_decorator import TrackingDecorator
+
+key_figure_group = "berlin-lor-points-of-interest"
+
+statistics_paths = [
+    f"{key_figure_group}-2024-01",
+]
+
+statistics_names = [
+    "doctors",
+]
+
+
+@TrackingDecorator.track_time
+def blend_data_details(source_path, results_path, clean=False, quiet=False):
+    # Iterate over files
+    for subdir, dirs, files in sorted(os.walk(source_path)):
+
+        # Make results path
+        subdir = subdir.replace(f"{source_path}/", "")
+        os.makedirs(os.path.join(results_path, subdir), exist_ok=True)
+
+        for file_name in [file_name for file_name in sorted(files) if file_name.endswith("-details.csv")]:
+            source_file_path = os.path.join(source_path, subdir, file_name)
+            blend_details_into_geojson(source_file_path, clean=clean, quiet=quiet)
+
+
+def blend_details_into_geojson(source_file_path, clean, quiet):
+    source_file_name, source_file_extension = os.path.splitext(source_file_path)
+    target_file_path = f"{source_file_name}.geojson"
+
+    geojson = {
+        "type": "FeatureCollection",
+        "features": []
+    }
+
+    if clean or not os.path.exists(target_file_path):
+        dataframe_details = read_csv_file(source_file_path)
+
+        for _, detail in dataframe_details.iterrows():
+            geojson["features"].append({
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [float(detail["lon"]), float(detail["lat"])]
+                },
+                "properties": {
+                    "id": detail["id"],
+                    "name": detail["name"],
+                    "street": detail["street"],
+                    "zip-code": detail["zip_code"],
+                    "city": detail["city"],
+                    "planning-area-id": int(detail["planning_area_id"])
+                }
+            })
+
+        # Write geojson file
+        write_geojson_file(
+            file_path=os.path.join(target_file_path),
+            statistic_name=f"{os.path.basename(target_file_path)}",
+            geojson_content=geojson,
+            clean=clean,
+            quiet=quiet
+        )
+
+
+def read_csv_file(file_path):
+    if os.path.exists(file_path):
+        with open(file_path, "r") as csv_file:
+            return pd.read_csv(csv_file, dtype={"id": "str"}, header=0, index_col=False).fillna("")
+    else:
+        return None
+
+
+def write_geojson_file(file_path, statistic_name, geojson_content, clean, quiet):
+    if not os.path.exists(file_path) or clean:
+
+        # Make results path
+        path_name = os.path.dirname(file_path)
+        os.makedirs(os.path.join(path_name), exist_ok=True)
+
+        with open(file_path, "w", encoding="utf-8") as geojson_file:
+            json.dump(geojson_content, geojson_file, ensure_ascii=False)
+
+            if not quiet:
+                print(f"✓ Blend data from {statistic_name} into {os.path.basename(file_path)}")
+    else:
+        print(f"✓ Already exists {os.path.basename(file_path)}")
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from lib.transform.data_aggregator import aggregate
 from lib.transform.data_blender import blend_data
 from lib.transform.data_copier import copy_data
 from lib.transform.data_csv_converter import convert_data_to_csv
+from lib.transform.data_details_blender import blend_data_details
 from lib.transform.data_filterer import filter_data
 from lib.transform.data_lor_area_assigner import assign_lor_area
 
@@ -65,6 +66,10 @@ def main(argv):
 
     aggregate(source_path=workspace_path, results_path=workspace_path, clean=clean, quiet=quiet)
     blend_data(source_path=workspace_path, results_path=workspace_path, clean=clean, quiet=quiet)
+
+    # Details
+
+    blend_data_details(source_path=workspace_path, results_path=workspace_path, clean=clean, quiet=quiet)
 
 
 if __name__ == "__main__":
