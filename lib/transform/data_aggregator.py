d7eeb319b6d1c36352ab5a73176840cec0474c7e
Florian Schwanz <florian.schwanz@interoberlin.de>
2024-04-04 08:06:30 +0200

    fix: timestamp in data aggregator

--- lib/transform/data_aggregator.py
+++ lib/transform/data_aggregator.py
@@ -1,6 +1,6 @@
 import json
 import os
-from datetime import datetime
+import re
 from functools import reduce
 
 import pandas as pd
@@ -87,48 +87,55 @@ statistic_properties = [
 
 @TrackingDecorator.track_time
 def aggregate(source_path, results_path, clean=False, quiet=False):
-    timestamp = datetime.now().strftime("%Y-%m")
+    # Iterate over files
+    for subdir, dirs, files in sorted(os.walk(source_path)):
+        subdir = subdir.replace(f"{source_path}", "")
 
-    target_file_path = os.path.join(results_path, f"{key_figure_group}-{timestamp}",
-                                    f"{key_figure_group}-{timestamp}.csv")
+        if "points-of-interest-2" in subdir:
+            year = re.findall(r"\b\d{4}\b", subdir)[0]
+            month = re.findall(r"\b\d{2}\b", subdir)[0]
+            timestamp = f"{year}-{month}"
 
-    if not os.path.exists(target_file_path) or clean:
+            target_file_path = os.path.join(results_path, f"{key_figure_group}-{timestamp}",
+                                            f"{key_figure_group}-{timestamp}.csv")
 
-        dataframes_grouped = []
+            if not os.path.exists(target_file_path) or clean:
 
-        for statistic_name in statistic_properties:
-            # Read source file
-            source_file_path = os.path.join(source_path, f"{key_figure_group}-{timestamp}",
-                                            f"{key_figure_group}-{statistic_name.replace('_', '-')}-{timestamp}-details.csv")
-            dataframe_details = read_csv_file(source_file_path)
+                dataframes_grouped = []
 
-            try:
-                dataframe_grouped = dataframe_details.groupby("planning_area_id").agg(
-                    count=("planning_area_id", "size"),
-                ).reset_index() \
-                    .sort_values(by="planning_area_id") \
-                    .rename(columns={"planning_area_id": "id"}) \
-                    .assign(id=lambda df: df["id"].astype(int).astype(str).str.zfill(8)) \
-                    .assign(count=lambda df: df["count"].astype(pd.Int64Dtype(), errors="ignore")) \
-                    .rename(columns={"count": statistic_name})
-                dataframes_grouped.append(dataframe_grouped)
-            except Exception as e:
-                print(f"✗️ Exception: {str(e)}")
+                for statistic_name in statistic_properties:
+                    # Read source file
+                    source_file_path = os.path.join(source_path, f"{key_figure_group}-{timestamp}",
+                                                    f"{key_figure_group}-{statistic_name.replace('_', '-')}-{timestamp}-details.csv")
+                    dataframe_details = read_csv_file(source_file_path)
 
-        dataframe = reduce(lambda left, right: pd.merge(left, right, on="id", how="outer"), dataframes_grouped)
-        dataframe.fillna(0, inplace=True)
+                    try:
+                        dataframe_grouped = dataframe_details.groupby("planning_area_id").agg(
+                            count=("planning_area_id", "size"),
+                        ).reset_index() \
+                            .sort_values(by="planning_area_id") \
+                            .rename(columns={"planning_area_id": "id"}) \
+                            .assign(id=lambda df: df["id"].astype(int).astype(str).str.zfill(8)) \
+                            .assign(count=lambda df: df["count"].astype(pd.Int64Dtype(), errors="ignore")) \
+                            .rename(columns={"count": statistic_name})
+                        dataframes_grouped.append(dataframe_grouped)
+                    except Exception as e:
+                        print(f"✗️ Exception: {str(e)}")
 
-        # Write csv file
-        if dataframe.shape[0] > 0:
-            dataframe.to_csv(target_file_path, index=False)
-        if not quiet:
-            print(f"✓ Aggregate into {os.path.basename(target_file_path)}")
-        else:
-            if not quiet:
-                print(dataframe.head())
-                print(f"✗️ Empty {os.path.basename(target_file_path)}")
-    else:
-        print(f"✓ Already aggregate into {os.path.basename(target_file_path)}")
+                dataframe = reduce(lambda left, right: pd.merge(left, right, on="id", how="outer"), dataframes_grouped)
+                dataframe.fillna(0, inplace=True)
+
+                # Write csv file
+                if dataframe.shape[0] > 0:
+                    dataframe.to_csv(target_file_path, index=False)
+                if not quiet:
+                    print(f"✓ Aggregate into {os.path.basename(target_file_path)}")
+                else:
+                    if not quiet:
+                        print(dataframe.head())
+                        print(f"✗️ Empty {os.path.basename(target_file_path)}")
+            else:
+                print(f"✓ Already aggregate into {os.path.basename(target_file_path)}")
 
 
 def read_csv_file(file_path):
